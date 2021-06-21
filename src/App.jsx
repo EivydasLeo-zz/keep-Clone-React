30d52df3473ee7b941846b084a95fb2e3477b4db
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-21 15:33:06 +0300

    modal close on backdrop

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import AppAside from './components/appAside/appAside';
 import AppAddNote from './components/appAddNote/appAddNote';
 import AppNoteList from './components/appNoteList/appNoteList';
 import './App.css';
+import Modal from './components/modal/modal';
 class App extends Component {
   state = {
     notes: [],
     noteId: 1,
+    showModal: false,
   };
 
   handleNewNote = (newNote) => {
@@ -21,9 +23,15 @@ class App extends Component {
     return { _id: this.state.noteId, ...newNote, isPinned: false };
   }
 
+  handleModal = () => {
+    console.log('modal ???');
+    this.setState({ showModal: !this.state.showModal });
+  };
+
   render() {
     return (
       <div className="App">
+        {this.state.showModal && <Modal onToggleModal={this.handleModal}></Modal>}
         <AppNavbar />
         <AppAside />
         <AppAddNote onNewNote={this.handleNewNote} />
